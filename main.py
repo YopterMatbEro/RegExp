680e28f45d43a08fcbf68e2c72cf6413bf5224e7
YopterMatbEro <yopter2013@gmail.com>
2023-06-19 18:09:39 +0300

    shlifovka

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pprint import pprint
 import re
 # Читаем адресную книгу в формате CSV в список contacts_list:
 import csv
-from operations import correction2
+from operations import correction_of_contacts
 
 def main():
     with open("phonebook_raw.csv", encoding='utf-8') as f:
@@ -11,12 +11,13 @@ def main():
     # pprint(contacts_list)
 
     # 1. Выполните пункты 1-3 задания.
-    correction2(contacts_list[1:])
+    changed_list = correction_of_contacts(contacts_list[1:])  # исключаем первую строку, т.к. она статична
+    print(*changed_list, sep='\n')
 
-    # print(f'''{changed_list[0][0]:^20} | {changed_list[0][1]:^20} | {changed_list[0][2]:^20} | {changed_list[0][3]:^10}\
-    #  | {changed_list[0][4]:^143} | {changed_list[0][5]:^27}| {changed_list[0][6]:^21}''')
+    # print(f'''{contacts_list[0][0]:^20} | {contacts_list[0][1]:^20} | {contacts_list[0][2]:^20} | {contacts_list[0][3]:^10}\
+    #  | {contacts_list[0][4]:^143} | {contacts_list[0][5]:^27}| {contacts_list[0][6]:^21}''')
     # print('+' + '-'*20 + '+' + '-'*22 + '+' + '-'*22 + '+' + '-'*18 + '+' + '-'*145 + '+' + '-'*28 + '+' + '-'*21)
-    # for l, f, s, o, p, ph, e in changed_list[1:]:
+    # for l, f, s, o, p, ph, e in changed_list:
     #     print(f'{l:^20} | {f:^20} | {s:^20} | {o:^16} | {p:^143} | {ph:^26} | {e:^21}')
 
     # 2. Сохраните получившиеся данные в другой файл.
--- operations.py
+++ operations.py
@@ -1,44 +1,52 @@
 import re
 from pprint import pprint
 
-def change_phone(id, id1, change_contacts_list, pattern, contacts_list):
+def change_phone(id, id1, change_contacts_list, pattern, contacts_list, match):
     if change_contacts_list[id1][5]:  # полный телефон (группа 6, в списке индекс 5)
         if change_contacts_list[id1][12]:  # добавочный номер
             subst_pattern = r"+7(\8)\9-\10-\11 \12.\13"
             change_contacts_list[id1][5] = pattern.sub(subst_pattern, contacts_list[id])
-        # elif change_contacts_list[id][12] == '':
+            [change_contacts_list[id1].remove(match.group(i)) for i in [7, 8, 9, 10, 11]]
         else:
             subst_pattern = r"+7(\8)\9-\10-\11"
             change_contacts_list[id1][5] = pattern.sub(subst_pattern, contacts_list[id])
+            [change_contacts_list[id1].remove(match.group(i)) for i in [7, 8, 9, 10, 11, 12, 13]]
 
 
-def correction2(contacts_list):
+def correction_of_contacts(contacts_list):
     change_contacts_list = []
+    count = 0  # кол-во объединённых контактов в итоговом списке
     for id, item in enumerate(contacts_list):  # корректируем каждую строку по очереди
         contacts_list[id] = ','.join(item)
         pattern = re.compile(
             '^(\w+)?[\s,]+(\w+)?[\s,]+(\w+)?[\s,]+(\w+)?[s,]*([a-zA-Zа-яА-Я\s]*[–]?[a-zA-Zа-яА-Я\s]*)[\s*,](([+]?\d*)[\s(]*(\d{3})[\s)-]*(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})[\s+(]*([\w+]+)?[\s.]*(\d+)?)?[)]?[\s,]([a-zA-Zа-яА-Я.@0-9]+)?')
         match = pattern.search(contacts_list[id])
+
         # Добавляем в виде списка в уже отсортированном, по группам, виде (Ф,И,О, и т.д.)
         # 1. ищем индекс возможных совпадений в конечном списке
         id1 = [change_contacts_list.index(change_contacts_list[i]) for i in range(len(change_contacts_list))
                if match.group(1) in change_contacts_list[i][0]]
-        count = 0
-        if id1:
-            count += 1
+
+        if id1:  # [n] - index уже имеющегося контакта с этой фамилией
+            count += 1  # + объединённый контакт, для вычитания из id и попадания по верному индексу в новом списке
+            # поиск и заполнение новых данных, отсутствующих у первичного контакта
             for elem in change_contacts_list[id1[0]]:
-                if elem != '':
+                if elem:
                     continue
                 else:
+                    # определяем позицию недостающих данных
                     position = change_contacts_list[id1[0]].index(elem)
-                    change_contacts_list[id1[0]][position] = match.group(position + 1)
-            change_phone(id, id1[0], change_contacts_list, pattern, contacts_list)
-        # if match.group(1) in [change_contacts_list[i][0] for i in range(len(change_contacts_list))]:
-        #     index = change_contacts_list.index(match.group(1))
-        #     print(index)
-        # else:
+                    change_contacts_list[id1[0]][position] = match.group(position + 1)  # добавляем из группы
+            # подгоняем номер телефона под установленный шаблон, в случае, если он не прописан
+            if change_contacts_list[id1[0]][5]:
+                continue
+            else:
+                change_phone(id, id1[0], change_contacts_list, pattern, contacts_list, match)
         # 2. добавляем, если совпадений нет
-        else:
+        else:  # [] - not id1
             change_contacts_list.append([*match.groups()])
-            change_phone(id - count, id, change_contacts_list, pattern, contacts_list)
-    pprint(change_contacts_list)
+            # change_contacts_list.append([match.group(1), match.group(2), match.group(3), match.group(4), match.group(5),
+            #                              match.group(6), match.group(12), match.group(13), match.group(14)])
+            change_phone(id, id - count, change_contacts_list, pattern, contacts_list, match)
+
+    return change_contacts_list
